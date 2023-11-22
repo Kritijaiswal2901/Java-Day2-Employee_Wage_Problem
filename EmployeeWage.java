261fa5d9277e82bf5ab380d511dc77dcd25b95a3
Kriti Jaiswal <kritijaiswal2901@yahoo.com>
2023-11-22 19:17:24 +0530

    Part time

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -5,17 +5,27 @@ public class EmployeeWage {
         return empType == 1;
     }
 
-    private static int dailyWage() {
+    private static int dailyWage(int todayDailyHour) {
         final int DAILY_WAGE_PER_HOUR = 20;
-        final int TOTAL_DAILY_HOUR = 8;
-        return DAILY_WAGE_PER_HOUR * TOTAL_DAILY_HOUR;
+        return DAILY_WAGE_PER_HOUR * todayDailyHour;
+    }
+
+    private static Boolean isPartTime() {
+        int empType = (int) (Math.floor(Math.random() * 10) % 2);
+        return empType == 1;
     }
 
     public static void main(String[] agrs) {
         System.out.println("Welcome to employee wage computation problem");
+        final int FULL_TIME_DAILY_HOUR = 8;
+        final int PART_TIME_TIME_DAILY_HOUR = 4;
         if(checkAttendance()) {
             System.out.println("Employee is Present");
-            System.out.println("Daily Wage: " + dailyWage());
+            if(isPartTime()) {
+                System.out.println("Daily Wage (Part Time): " + dailyWage(PART_TIME_TIME_DAILY_HOUR));
+            } else {
+                System.out.println("Daily Wage (Full Time): " + dailyWage(FULL_TIME_DAILY_HOUR));
+            }
         } else {
             System.out.println("Employee is Absent");
             System.out.println("Daily Wage: " + 0);
