5632c03566d1a886bd64f207848148de4b867a09
Kriti Jaiswal <kritijaiswal2901@yahoo.com>
2023-11-22 19:41:46 +0530

    Monthly Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -15,23 +15,27 @@ public class EmployeeWage {
         return empType;
     }
 
+    private static int monthlyWage(int dailyWage) {
+        final int NO_OF_DAYS = 20;
+        return NO_OF_DAYS * dailyWage;
+    }
+
     public static void main(String[] agrs) {
         System.out.println("Welcome to employee wage computation problem");
         final int FULL_TIME_DAILY_HOUR = 8;
         final int PART_TIME_DAILY_HOUR = 4;
         if(checkAttendance()) {
             System.out.println("Employee is Present");
-            switch (isPartTime()) {
-                case 0:
-                    System.out.println("Daily Wage (Full Time): " + dailyWage(FULL_TIME_DAILY_HOUR));
-                    break;
-                case 1:
-                    System.out.println("Daily Wage (Part Time): " + dailyWage(PART_TIME_DAILY_HOUR));
-                    break;
-            }
         } else {
             System.out.println("Employee is Absent");
-            System.out.println("Daily Wage: " + 0);
+        }
+        
+        if(isPartTime() == 0) {
+            System.out.println("Daily Wage (Full Time): " + dailyWage(FULL_TIME_DAILY_HOUR));
+            System.out.println("Monthly Wage (Full Time): " + monthlyWage(dailyWage(FULL_TIME_DAILY_HOUR)));
+        } else {
+            System.out.println("Daily Wage (Part Time): " + dailyWage(PART_TIME_DAILY_HOUR));
+            System.out.println("Monthly Wage (Part Time): " + monthlyWage(dailyWage(PART_TIME_DAILY_HOUR)));
         }
     }
 }
