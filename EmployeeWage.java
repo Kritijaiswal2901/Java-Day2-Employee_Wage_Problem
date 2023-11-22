09a9f439b28e3ca94168fb49f19618e9886ae4e5
Kriti Jaiswal <kritijaiswal2901@yahoo.com>
2023-11-22 19:10:35 +0530

    Daily Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,12 +4,21 @@ public class EmployeeWage {
         int empType = (int) (Math.floor(Math.random() * 10) % 2);
         return empType == 1;
     }
+
+    private static int dailyWage() {
+        final int DAILY_WAGE_PER_HOUR = 20;
+        final int TOTAL_DAILY_HOUR = 8;
+        return DAILY_WAGE_PER_HOUR * TOTAL_DAILY_HOUR;
+    }
+
     public static void main(String[] agrs) {
         System.out.println("Welcome to employee wage computation problem");
         if(checkAttendance()) {
             System.out.println("Employee is Present");
+            System.out.println("Daily Wage: " + dailyWage());
         } else {
             System.out.println("Employee is Absent");
+            System.out.println("Daily Wage: " + 0);
         }
     }
 }
